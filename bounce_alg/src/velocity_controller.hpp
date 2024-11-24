aed30efb4b92e6426ee9bed368953ed60437fa50
Tomas-412 <151516875+Tomas-412@users.noreply.github.com>
2024-11-23 20:51:25 -0600

    velocity_controller.hpp parameters in constructor

--- bounce_alg/src/velocity_controller.hpp
+++ bounce_alg/src/velocity_controller.hpp
@@ -8,12 +8,13 @@ class VelocityController
 {
 public:
     VelocityController(
-        double stop_thresh, double slow_thresh, double obstacle_zone_y)
-        : stop_threshold_(stop_thresh), slow_threshold_(slow_thresh),
-          obstacle_zone_y_(obstacle_zone_y) {}
+        double stop_thresh, double slow_thresh, double obstacle_zone_y
+        double linear_vel, double angular_vel)
+        : stop_threshold_(stop_thresh), slow_threshold_(slow_thresh), obstacle_zone_y_(obstacle_zone_y),
+        linear_vel_param_(linear_vel), angular_vel_param_(angular_vel) {}
 
     // Function to calculate the velocity command
-    geometry_msgs::msg::Twist calculateVelocity(double nearestPointDistance, const pcl::PointXYZ &nearestPoint, double linear_vel_param, double angular_vel_param)
+    geometry_msgs::msg::Twist calculateVelocity(double nearestPointDistance, const pcl::PointXYZ &nearestPoint)
     {
 
         geometry_msgs::msg::Twist cmd_vel;
@@ -25,21 +26,19 @@ public:
         else if (nearestPointDistance < stop_threshold_ && nearestPoint.x > 0 && abs(nearestPoint.y) < obstacle_zone_y_)
         {
             // Stop and turn
-            angular_vel = (nearestPoint.y > 0) ? -angular_vel_param : angular_vel_param; // Turn right if obstacle on the left, and vice versa
+            angular_vel = (nearestPoint.y > 0) ? -angular_vel_param_ : angular_vel_param_; // Turn right if obstacle on the left, and vice versa
         }
         else if (nearestPointDistance < slow_threshold_)
         {
             // Slow down and adjust direction
-            linear_vel = linear_vel_param * (nearestPointDistance / slow_threshold_);
-            angular_vel = -angular_vel_param * 0.5 * (nearestPoint.y / slow_threshold_); // Proportional steering
+            linear_vel = linear_vel_param_ * (nearestPointDistance / slow_threshold_);
+            angular_vel = -angular_vel_param_ * 0.5 * (nearestPoint.y / slow_threshold_); // Proportional steering to right if obstacle on the left, and vice versa
         }
         else
         {
             // No critical obstacle, proceed with default parameters
             linear_vel = linear_vel_param;
         }
-
-        // if (angular_vel > angular_vel_param) {angular_vel = angular_vel_param}
 
         // Set velocities in the Twist message
         cmd_vel.linear.x = linear_vel;
@@ -49,9 +48,8 @@ public:
     }
 
 private:
-    double stop_threshold_;
-    double slow_threshold_;
-    double obstacle_zone_y_;
+    double stop_threshold_, slow_threshold_, obstacle_zone_y_;
+    double linear_vel_param_, angular_vel_param_;
 };
 
 #endif // VELOCITY_CONTROLLER_HPP
